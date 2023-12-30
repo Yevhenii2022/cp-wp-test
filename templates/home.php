414f6cb35ff590e0a51b6bab5bb2219d2bef6f04
Yevhenii2022 <3starosta3@gmail.com>
2023-12-30 15:06:57 +0200

    add iso card

--- templates/home.php
+++ templates/home.php
@@ -25,6 +25,35 @@ get_header();
     </div>
   </section>
 
+  <section class="iso">
+
+    <div class="container">
+      <div class="iso-wrapper">
+        <h2 class="iso-title"><?php the_field('iso_title'); ?></h2>
+        <p class="iso__subtitle"><?php the_field('iso_text'); ?></p>
+      </div>
+
+      <ul class="iso__list">
+        <li>
+          <a href="#">
+            <article class="iso__card">
+              <div class="iso__card-wrapper">
+                <img class="iso__img" src="<?php echo get_template_directory_uri() ?>/assets/images/iso.png" alt="iso logo">
+                <svg class="iso__icon">
+                  <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#arrow"></use>
+                </svg>
+              </div>
+              <p class="iso__text">система менеджменту якості</p>
+            </article>
+          </a>
+        </li>
+      </ul>
+
+    </div>
+
+    </div>
+  </section>
+
 </main>
 
 <?php get_template_part('template-parts/form'); ?>
