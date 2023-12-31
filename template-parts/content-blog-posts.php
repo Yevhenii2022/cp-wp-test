4744f1b131c7615a70bd086ac0648f910833a997
litvinenkoiryna <litvinenkoiryna@gmail.com>
2023-12-31 02:57:27 +0200

    added svg

--- template-parts/content-blog-posts.php
+++ template-parts/content-blog-posts.php
@@ -6,8 +6,27 @@ get_header();
 ?>
 <section class="blog">
    <div class="container">
-
-   </div>    
+      <div class="blog__content">
+         <div class="blog__banner">
+            <h1 class="blog__title"></h1>
+         </div>
+         <div class="blog__wrapper">
+            <div class="blog__post">
+               <div class="blog_post-image"></div>
+               <h2 class="blog__post-time"></h2>
+               <p class="blog__post-title"></p>
+            </div>
+         </div>
+      </div>
+      <div class="blog__share">
+         <p class="blog__text"></p>
+         <ul class="blog__list">
+            <li class="blog__link"></li>
+            <li class="blog__link"></li>
+            <li class="blog__link"></li>
+         </ul>
+      </div>
+   </div>
 </section>
 
 
