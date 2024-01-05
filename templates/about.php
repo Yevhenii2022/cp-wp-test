58348bbbfc5dc3f10c838a3e539b50f3abfee64e
Yevhenii2022 <3starosta3@gmail.com>
2024-01-05 21:03:33 +0200

    add section banner

--- templates/about.php
+++ templates/about.php
@@ -4,8 +4,21 @@ Template Name: about
 */
 get_header();
 ?>
-<h2></h2>
+
+<main>
+  <section class="about-page">
+    <div class="container">
+      <h2 class="about-page__title"><?php the_field('about_baner_title'); ?></h2>
+
+      <div class="iso-wrapper">
+        <p class="iso-title"><?php the_field('iso_title'); ?></p>
+        <p class="iso__subtitle"><?php the_field('iso_text'); ?></p>
+      </div>
 
 
+    </div>
+  </section>
+
+</main>
 
 <?php get_footer(); ?>
