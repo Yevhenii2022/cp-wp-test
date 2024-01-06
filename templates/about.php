47e57e15e0cbe1a956c6f4310deab381ab845e1a
Yevhenii2022 <3starosta3@gmail.com>
2024-01-06 18:15:19 +0200

    add principles section

--- templates/about.php
+++ templates/about.php
@@ -47,19 +47,23 @@ get_header();
     </div>
   </section>
 
-  <section class="experience">
+  <section class="expectation">
     <div class="container">
-
       <div class="expectation-wrapper">
-        <img class="expectation__img" src="https://test4.cursorpointer.agency/wp-content/uploads/2024/01/about.webp" alt="conversation at the table">
+        <?php
+        $img = get_field('expectation_img');
+        ?>
+        <img class="expectation__img" src='<?php echo $img['url']; ?>' alt='<?php echo $img['alt']; ?>' />
         <div>
           <h2 class="expectation__title"><?php the_field('expectation_title'); ?></h2>
 
           <?php
-          $expectation_list = get_field('expectation_list');
+          // Отримуємо ID конкретного запису типу "iso" 
+          $iso_id = 39;
+          $expectation_list = get_field('expectation_list', $iso_id);
+
           if ($expectation_list) {
             echo '<ul class="expectation__list">';
-
             foreach ($expectation_list as $item) {
               $expectation_item = $item['expectation_item'];
               if ($expectation_item) {
@@ -69,10 +73,83 @@ get_header();
             echo '</ul>';
           }
           ?>
-
-          <h3 class="expectation__subtitle"><?php the_field('expectation_subtitle'); ?></h3>
+          <h3 class="expectation__subtitle"><?php echo esc_html(get_field('expectation_subtitle', $iso_id)); ?></h3>
         </div>
       </div>
+    </div>
+  </section>
+
+  <section class="principles">
+    <div class="container">
+
+      <div class="principles-wrapper">
+
+
+        <div class="principles-wrapper-text">
+          <?php
+          $img1 = get_field('principles_img1');
+          ?>
+          <img class="principles__img img1" src='<?php echo $img1['url']; ?>' alt='<?php echo $img['alt']; ?>' />
+          <?php
+          $img2 = get_field('principles_img2');
+          ?>
+          <img class="principles__img img2" src='<?php echo $img2['url']; ?>' alt='<?php echo $img['alt']; ?>' />
+          <?php
+          $img3 = get_field('principles_img3');
+          ?>
+          <img class="principles__img img3" src='<?php echo $img3['url']; ?>' alt='<?php echo $img['alt']; ?>' />
+          <?php
+          $img4 = get_field('principles_img4');
+          ?>
+          <img class="principles__img img4" src='<?php echo $img4['url']; ?>' alt='<?php echo $img['alt']; ?>' />
+        </div>
+
+        <div class="principles-wrapper-text">
+
+          <h3 class="principles__title"><?php the_field('principles_title'); ?></h3>
+
+          <?php
+          $principles_list = get_field('principles_list');
+          if ($principles_list) {
+            echo '<ul class="principles__list">';
+
+            foreach ($principles_list as $item) {
+              $principles_subtitle = $item['principles_subtitle'];
+              $principles_text = $item['principles_text'];
+
+              // Змінено $principles_item на $item
+              if ($item) {
+                echo '<li class="principles__item">
+                <p class="principles__subtitle">' . esc_html($principles_subtitle) . '</p>
+                <p class="principles__text">' . esc_html($principles_text) . '</p>
+            </li>';
+              }
+            }
+            echo '</ul>';
+          }
+          ?>
+
+
+        </div>
+
+
+
+      </div>
+    </div>
+  </section>
+
+  <section class="work">
+    <div class="container">
+      <div class="work-wraper">
+        <div class="work-flex">
+          <h2 class="work__title work-color"><?php the_field('work_title_first'); ?></h2>
+          <h2 class="work__title"><?php the_field('work_title_second'); ?></h2>
+        </div>
+        <p class="work__text"><?php the_field('work_text'); ?></p>
+      </div>
+
+      <?php get_template_part('template-parts/tooltip'); ?>
+
     </div>
   </section>
 
